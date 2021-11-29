f0fbac35acb1bbafd52d192a789bc7f76b43058d
Mauriney <maurineyy@gmail.com>
2021-11-29 09:18:02 -0500

    basic configuration - finished

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const connection = require("./database/database")
 
 app.set('view engine', 'ejs')
 
+// Static
 app.use(express.static('public'))
 
-app.use(bodyParser.urlencoded({extends: false}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
+// app.use(express.urlencoded({extends: false}))
+// app.use(express.json())
+
+
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("conexão realizada com sucesso!")
+    }).catch((error) => {
+        console.log(error)
+    })
 
 app.get("/", (req, res) => {
     res.render("index")

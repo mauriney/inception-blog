a374bcbd787a01d9f2b2b96990c059de00d870a0
Mauriney <maurineyy@gmail.com>
2021-11-29 09:44:22 -0500

    Routes defined

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const app = express()
 const bodyParser = require("body-parser")
 const connection = require("./database/database")
 
+const CategoriesController = require("./categories/CategoriesController")
+const ArticlesController = require("./articles/ArticlesController")
+
 app.set('view engine', 'ejs')
 
 // Static
 app.use(express.static('public'))
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-// app.use(express.urlencoded({extends: false}))
-// app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 
 //Database
@@ -23,6 +23,11 @@ connection
     }).catch((error) => {
         console.log(error)
     })
+
+//Informando que vai utilizar a rota de CategoriesController
+app.use("/", CategoriesController)
+//Informando que vai utilizar a rota de ArticlesController
+app.use("/", ArticlesController)
 
 app.get("/", (req, res) => {
     res.render("index")

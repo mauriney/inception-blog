48a630718b8a8d2a77bf71baa3017b3099cf1bba
Mauriney <maurineyy@gmail.com>
2021-11-30 09:32:14 -0500

    category form modifications

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get("/categorias", (req, res) => {
-    res.send("Rota de categorias")
-})
+router.get("/admin/categories/new",(req,res) => {
+    res.render("/admin/categories/new")
+});
 
 module.exports = router;

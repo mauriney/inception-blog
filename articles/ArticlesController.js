ac6de8f21dad88442b675a37ce8e96d3eedee011
Mauriney <maurineyy@gmail.com>
2021-12-04 11:12:00 -0500

    CRUD - Articles: In progress

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,13 +1,35 @@
 const express = require("express")
 const router = express.Router()
+const Category = require("../categories/Category")
+const Article = require("./Article")
+const slugify = require("slugify")
 
-router.get("/articles", (req, res) => {
-    res.send("Rota de Artigos")
+router.get("/admin/articles", (req, res) => {
+    // res.send("Rota de Artigos")
+    res.render("admin/articles/index")
 })
 
+//Rota de novo artigo
 router.get("/admin/articles/new", (req,res) => {
-    res.send("Rota para criar um novo artigo")
+    Category.findAll().then(categories => {//lista as categorias na pagina de artigos
+        res.render("admin/articles/new", {categories: categories})//renderiza a pagina de novo artigo || {categories renderizada no artigo}
+    })
 });
 
+//Rota para salvar artigo
+router.post("/articles/save", (req, res) => {
+    let title = req.body.title
+    let body = req.body.body
+    let category = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles")
+    })
+})
 
 module.exports = router;
